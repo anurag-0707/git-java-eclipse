85e18e2017cf5e761e20f56cdd7075bf30a62bea
Anurag <anu.s1104@gmail.com>
2018-04-30 19:25:53 +0530

    ThrowTest Remove Comment

--- src/ExceptionHandling/ThrowTest.java
+++ src/ExceptionHandling/ThrowTest.java
@@ -3,9 +3,10 @@ package ExceptionHandling;
 public class ThrowTest {
 
 	public static void main(String[] args) {
+		System.out.println("Hello");
 		System.out.println(10 / 0);
 		throw new ArithmeticException();
-//		System.out.println("Hello");
+		
 
 	}
 
